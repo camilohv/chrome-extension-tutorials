72bf8404a369bcdf885569f1e585b033621db3bd
camilohv <camilohvillavicencio@gmail.com>
2022-11-14 15:57:53 -0500

    added temporary style changing

--- chrome-dom/scripts/content.js
+++ chrome-dom/scripts/content.js
@@ -3,12 +3,21 @@ chrome.runtime.onMessage.addListener( function(request, sender, sendResponse) {
     console.log("something happening from the extension");
     var data = request.data || {};
 
-    document.body.style.backgroundColor = "red"
+    console.log(data);
 
+    if (data == "red"){
+        document.body.style.backgroundColor = "red"
+    }
+
+    if (data == "blue"){
+        document.body.style.backgroundColor = "blue"
+    }
+
+    /*
     var linksList = document.querySelectorAll('a');
     [].forEach.call(linksList, function(header) {
         header.innerHTML = request.data;
-    });
+    });*/
     sendResponse({data: data, success: true});
 });
 
--- chrome-dom/scripts/extension.js
+++ chrome-dom/scripts/extension.js
@@ -2,6 +2,9 @@ document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('status').textContent = "Extension loaded";
     var button = document.getElementById('changelinks');
 
+    var btnRed = document.getElementById('red');
+    var btnBlue = document.getElementById('blue');
+
     button.addEventListener('click', function () {
 
         $('#status').html('Clicked change links button');
@@ -24,5 +27,35 @@ document.addEventListener('DOMContentLoaded', function() {
             });
         });
     });
+
+    btnRed.addEventListener('click', function () {
+        // Finds the current active browser tab
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+
+            // Allows program to pass some data from popup script to content scripts
+            chrome.tabs.sendMessage(tabs[0].id, {data: "red"}, function(response) {  
+                $('#status').html('changed data in page');
+                console.log('red');
+            });
+        });
+    });
+
+    btnBlue.addEventListener('click', function () {
+        // Finds the current active browser tab
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+
+            // Allows program to pass some data from popup script to content scripts
+            chrome.tabs.sendMessage(tabs[0].id, {data: "blue"}, function(response) {  
+                $('#status').html('changed data in page');
+                console.log('blue');
+            });
+        });
+    });
+
+
+
+
+
+
 });
 
