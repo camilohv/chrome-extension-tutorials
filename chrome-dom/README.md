e688ec6e05b54fd9aab4328171ccdd9586f4b749
camilohv <camilohvillavicencio@gmail.com>
2022-11-14 16:02:19 -0500

    updated README

--- chrome-dom/README.md
+++ chrome-dom/README.md
@@ -1,4 +1,8 @@
-# chrome-ext-sample
-Sample chrome extension that replaces all links with given text
+# chrome-dom
+Sample chrome extension alters the styling of a webpage
 
 Made to learn about the way extensions interact with the DOM as practice for a bigger project.
+
+# To Do:
+
+- Use chrome.storage to save user settings
--- chrome-dom/scripts/extension.js
+++ chrome-dom/scripts/extension.js
@@ -5,29 +5,6 @@ document.addEventListener('DOMContentLoaded', function() {
     var btnRed = document.getElementById('red');
     var btnBlue = document.getElementById('blue');
 
-    button.addEventListener('click', function () {
-
-        $('#status').html('Clicked change links button');
-
-        var text = $('#linkstext').val();
-
-        if (!text) {
-            $('#status').html('Invalid text provided');
-            return;
-        }
-
-        // Finds the current active browser tab
-
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-
-            // Allows program to pass some data from popup script to content scripts
-            chrome.tabs.sendMessage(tabs[0].id, {data: text}, function(response) {  
-                $('#status').html('changed data in page');
-                console.log('success');
-            });
-        });
-    });
-
     btnRed.addEventListener('click', function () {
         // Finds the current active browser tab
         chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
@@ -51,6 +28,30 @@ document.addEventListener('DOMContentLoaded', function() {
             });
         });
     });
+    
+/*
+    button.addEventListener('click', function () {
+
+        $('#status').html('Clicked change links button');
+
+        var text = $('#linkstext').val();
+
+        if (!text) {
+            $('#status').html('Invalid text provided');
+            return;
+        }
+
+        // Finds the current active browser tab
+
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+
+            // Allows program to pass some data from popup script to content scripts
+            chrome.tabs.sendMessage(tabs[0].id, {data: text}, function(response) {  
+                $('#status').html('changed data in page');
+                console.log('success');
+            });
+        });
+    });*/
 
 
 
